7b5ea69cd67d30e25d010f475ca5efd67d61980f
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-10-29 10:34:28 +0200

    test1

--- Demo0.java
+++ Demo0.java
@@ -2,8 +2,8 @@
 // (Same program as shown in lecture 1-1).
 public class Demo0 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
-        int i = 1;
+		// Declares an int variable, and sets it to 0
+        int i = 0;
         while(i<6){
          	System.out.println(i);
          	i = i + 1;
