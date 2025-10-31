426d018c633189884a4df5a9d6b23db58d052ac1
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-10-31 17:38:52 +0200

    test1

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	     String name1 = args[0]; // Dana	
+	     String name2 = args[1]; // Rony
+	     String name3 = args[2]; // Shaked
+		 double amount = Double.parseDouble(args[3]); // bill amount
+		 double check = amount / 3; // each diner's share
+		 System.out.println("Dear " +name1 + ", " + name2 + " and " + name3 + ": pay '" + check + " Shekels each.");
+
 	}
 }
