66c835da46531d2b584b5365443da6c1b49bd666
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-11-03 19:28:23 +0200

    Final3

--- TimeFormat.java
+++ TimeFormat.java
@@ -3,24 +3,28 @@ public class TimeFormat {
 	public static void main(String[] args) {
 	String[] time = args[0].split(":");
   	int hours = Integer.parseInt(time[0]);
-  	int mins = Integer.parseInt(time[1]);
+  	String mins = time[1];
     
 	if(hours == 00){
 	int hour0 = hours + 12;
 	System.out.println(hour0 + ":" + mins + " AM");}
 	
 	else{
-	if (hours >= 12){
+	if (hours > 12){
 	   int hour = hours - 12;
 	   System.out.println(hour + ":" + mins + " PM");
 	 } 
 	else{
+		if(hours == 12){
+		System.out.println(hours + ":" + mins + " PM");
+	    }
+	    else{
 		System.out.println(hours + ":" + mins + " AM");
 
 	    }
     }
 	
 	}
+	}
 }
-
 
